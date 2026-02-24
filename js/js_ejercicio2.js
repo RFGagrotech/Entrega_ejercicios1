9997cd80fa114f05f2f558e353976f6a5ceafc13
Ruben F.G <rfgagrotech@gmail.com>
2026-02-24 00:27:33 +0000

    Ejercicio 2: adición de botón reset

--- js/js_ejercicio2.js
+++ js/js_ejercicio2.js
@@ -7,3 +7,15 @@ button.addEventListener("click", () => {
     clicks++;
     counterText.textContent = `Clics: ${clicks}`;
 });
+
+// Creo el botón de reset
+const resetBtn = document.createElement("button");
+//Escribo en el botón
+resetBtn.textContent = "Reset"; 
+//Añadir el boton al body
+document.body.appendChild(resetBtn);
+//Crear el evento del botón
+resetBtn.addEventListener("click", () => {
+    clicks = 0;
+    counterText.textContent = `Clicks: ${clicks}`;
+})
